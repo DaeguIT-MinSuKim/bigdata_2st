680f18411057696d5b82d867c22ea1f7bd2be990
NCG <NCG@DESKTOP-T6ENAEQ>
2017-01-06 16:29:56 +0900

    CustomerMapper 클래스 및 service 클래스 초기화

--- src/main/java/kr/or/dgit/bigdata/project/hairshop/mapper/CustomerMapper.java
+++ src/main/java/kr/or/dgit/bigdata/project/hairshop/mapper/CustomerMapper.java
@@ -5,10 +5,5 @@ import java.util.List;
 import kr.or.dgit.bigdata.project.hairshop.dto.Customer;
 
 public interface CustomerMapper {
-	void insertItem(Customer item);
-	void deleteItem(Customer idx);
-	void updateItem(Customer item);
-	Customer selectByNo(int idx);
-	Customer selectByName(Customer name);
-	List<Customer> selectByAll();
+	
 }
--- src/main/java/kr/or/dgit/bigdata/project/hairshop/service/CustomerService.java
+++ src/main/java/kr/or/dgit/bigdata/project/hairshop/service/CustomerService.java
@@ -1,13 +1,8 @@
 package kr.or.dgit.bigdata.project.hairshop.service;
 
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 
-import kr.or.dgit.bigdata.project.hairshop.dto.Customer;
 import kr.or.dgit.bigdata.project.hairshop.mapper.CustomerMapper;
-import kr.or.dgit.bigdata.project.hairshop.util.MyBatisSqlSessionFactory;
 
 public class CustomerService implements CustomerMapper{
 	private static final CustomerService instance = new CustomerService();
@@ -19,47 +14,6 @@ public class CustomerService implements CustomerMapper{
 	
 	private static final Logger logger = Logger.getLogger(CustomerService.class);
 	
-	@Override
-	public void insertItem(Customer item) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteItem(Customer idx) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateItem(Customer item) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Customer selectByNo(int idx) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Customer selectByName(Customer name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Customer> selectByAll() {
-		logger.debug("selectByAll()");
-		SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();
-		CustomerMapper studentDao =sqlSession.getMapper(CustomerMapper.class);
-		try {
-			return studentDao.selectByAll();
-		} finally {
-			sqlSession.close();
-		}
-	}
 	
 
 }
