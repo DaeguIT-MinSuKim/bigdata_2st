8d4e82371bee585bc2fe42bd459b3366978f1888
NCG <NCG@DESKTOP-T6ENAEQ>
2017-01-06 18:37:51 +0900

    merge 후 충돌 수정 완료

--- src/test/java/kr/or/dgit/bigdata/project/hairshop/CustomerServiceTest.java
+++ src/test/java/kr/or/dgit/bigdata/project/hairshop/CustomerServiceTest.java
@@ -24,7 +24,7 @@ public class CustomerServiceTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@Test
+	/*@Test
 	public void testInsertCustomer() {
 		GregorianCalendar cal1 = new GregorianCalendar(1983, 11, 10);
 		GregorianCalendar cal2 = new GregorianCalendar(2017, 1, 6);
@@ -39,7 +39,7 @@ public class CustomerServiceTest {
 		int res = customerService.insertCustomer(insCtm);
 		Assert.assertEquals(1, res);
 	}
-	/*@Test
+	@Test
 	public void testUdateCustomer() {
 		GregorianCalendar cal1 = new GregorianCalendar(1995, 11, 10);
 		GregorianCalendar cal2 = new GregorianCalendar(2017, 1, 6);
