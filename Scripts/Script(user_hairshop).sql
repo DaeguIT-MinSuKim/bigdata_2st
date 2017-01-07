081af18c789f4cf34a0b5a5fb0d404a9f37cbe12
rohgoon <rohgoon@PTAHKA>
2017-01-07 16:59:43 +0900

    biz 관련 스크립트 출력 확인

--- Scripts/Script(user_hairshop).sql
+++ Scripts/Script(user_hairshop).sql
@@ -8,5 +8,15 @@ from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo
 left outer join hairshop.customer c on b.cNo = c.cNo
 left outer join hairshop.event e on b.eNo = e.eNo; -- 월별 연도별 테이블
 
-select
-from 
+select (select count(*) from hairshop.customer tc where tc.cDel = false) as '총고객수',
+count(b.bNo) as '총영업수',
+sum(h.hPrice*(1-e.eDiscount)) as '총금액'
+from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo
+left outer join hairshop.customer c on b.cNo = c.cNo
+left outer join hairshop.event e on b.eNo = e.eNo;
+
+select h.hName as '스타일', count(b.hNo) as '주문 건수'
+from hairshop.biz b, hairshop.hairInfo h
+where b.hNo = h.hNo
+group by b.hNo; -- 헤어스타일별 주문수 카운트
+
