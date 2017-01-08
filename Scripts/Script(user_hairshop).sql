ba58f071800567f41b0c6255345790578b6d212d
rohgoon <rohgoon@PTAHKA>
2017-01-08 14:31:14 +0900

    스크립트내 한글명 영어로 수정

--- Scripts/Script(user_hairshop).sql
+++ Scripts/Script(user_hairshop).sql
@@ -8,14 +8,14 @@ from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo
 left outer join hairshop.customer c on b.cNo = c.cNo
 left outer join hairshop.event e on b.eNo = e.eNo; -- 월별 연도별 테이블
 
-select (select count(*) from hairshop.customer tc where tc.cDel = false) as '총고객수',
-count(b.bNo) as '총영업수',
-sum(h.hPrice*(1-e.eDiscount)) as '총금액'
+select (select count(*) from hairshop.customer tc where tc.cDel = false) as totalCustomer,
+count(b.bNo) as totalBiz,
+sum(h.hPrice*(1-e.eDiscount)) as totalMoney
 from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo
 left outer join hairshop.customer c on b.cNo = c.cNo
 left outer join hairshop.event e on b.eNo = e.eNo;
 
-select h.hName as '스타일', count(b.hNo) as '주문 건수'
+select h.hName as hairStyle, count(b.hNo) as sumOrder
 from hairshop.biz b, hairshop.hairInfo h
 where b.hNo = h.hNo
 group by b.hNo; -- 헤어스타일별 주문수 카운트
