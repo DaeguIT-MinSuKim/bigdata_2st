8c76ccd69cf469cf2c69945864258573fad05596
NCG <NCG@DESKTOP-T6ENAEQ>
2017-01-09 15:30:35 +0900

    고객검색 패널 디자인 추가

--- src/main/java/kr/or/dgit/bigdata/project/hairshop/ui/CustomerSearch.java
+++ src/main/java/kr/or/dgit/bigdata/project/hairshop/ui/CustomerSearch.java
@@ -1,14 +1,51 @@
 package kr.or.dgit.bigdata.project.hairshop.ui;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class CustomerSearch extends JPanel {
+	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public CustomerSearch() {
-
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel pnSearch = new JPanel();
+		add(pnSearch, BorderLayout.NORTH);
+		
+		JLabel lblSearch = new JLabel("검색 : ");
+		pnSearch.add(lblSearch);
+		
+		textField = new JTextField();
+		pnSearch.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnOk = new JButton("확인");
+		pnSearch.add(btnOk);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"고객번호", "고객명", "생년월일", "가입일자"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
 	}
 
 }
