293766da2d4c085edd4ac881f4bdf321f17e0ee1
NCG <NCG@DESKTOP-T6ENAEQ>
2017-01-10 16:27:18 +0900

    매핑 복구 완료

--- src/main/java/kr/or/dgit/bigdata/project/hairshop/mappers/BizMapper.java
+++ src/main/java/kr/or/dgit/bigdata/project/hairshop/mappers/BizMapper.java
@@ -10,19 +10,19 @@ import kr.or.dgit.bigdata.project.hairshop.dto.Biz;
 import kr.or.dgit.bigdata.project.hairshop.dto.Hairinfo;
 
 public interface BizMapper {
-	@Select("select b.bNo, b.bDate, h.hName, h.hPrice, e.eName, (h.hPrice*(1-e.eDiscount)) as resultPrice from hairshop.biz b left outer join hairshop.hairinfo h on b.bhNo = h.hNo left outer join hairshop.event e on b.beNo = e.eNo order by bNo desc")
+	@Select("select b.bNo, b.bDate, h.hName, h.hPrice, e.eName, (h.hPrice*(1-e.eDiscount)) as resultPrice from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo left outer join hairshop.event e on b.eNo = e.eNo order by bNo desc")
 	public List<Biz> selectBizAndHairInfoEvent();
 	
-	@Select("select b.bNo, b.bDate, h.hName, h.hPrice, e.eName, (h.hPrice*(1-e.eDiscount)) as resultPrice from hairshop.biz b left outer join hairshop.hairinfo h on b.bhNo = h.hNo left outer join hairshop.event e on b.beNo = e.eNo where b.bDate >= #{bDateStart} and b.bDate <=#{bDateEnd} order by bNo desc")
+	@Select("select b.bNo, b.bDate, h.hName, h.hPrice, e.eName, (h.hPrice*(1-e.eDiscount)) as resultPrice from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo left outer join hairshop.event e on b.eNo = e.eNo where b.bDate >= #{bDateStart} and b.bDate <=#{bDateEnd} order by bNo desc")
 	public List<Biz> selectBizAndHairInfoEventByDate(Biz biz); // xml 처럼 &gt; 을 사용하지 않면 안되고 부등호를 그대로 입력해야 인식됨.
 	
-	@Select("select b.bNo, b.bDate, c.cName, h.hName, e.eName, (h.hPrice*(1-e.eDiscount)) as resultPrice from hairshop.biz b left outer join hairshop.hairinfo h on b.bhNo = h.hNo left outer join hairshop.customer c on b.bcNo = c.cNo left outer join hairshop.event e on b.beNo = e.eNo order by b.bNo desc")
+	@Select("select b.bNo, b.bDate, c.cName, h.hName, e.eName, (h.hPrice*(1-e.eDiscount)) as resultPrice from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo left outer join hairshop.customer c on b.cNo = c.cNo left outer join hairshop.event e on b.eNo = e.eNo order by b.bNo desc")
 	public List<Biz> selectYearOrMonthAllFromBiz();
 	
-	@Select("select (select count(*) from hairshop.customer tc where tc.cDel = false) as totalCustomer, count(b.bNo) as totalBiz, sum(h.hPrice*(1-e.eDiscount)) as totalMoney from hairshop.biz b left outer join hairshop.hairinfo h on b.bhNo = h.hNo left outer join hairshop.customer c on b.bcNo = c.cNo left outer join hairshop.event e on b.beNo = e.eNo")
+	@Select("select (select count(*) from hairshop.customer tc where tc.cDel = false) as totalCustomer, count(b.bNo) as totalBiz, sum(h.hPrice*(1-e.eDiscount)) as totalMoney from hairshop.biz b left outer join hairshop.hairinfo h on b.hNo = h.hNo left outer join hairshop.customer c on b.cNo = c.cNo left outer join hairshop.event e on b.eNo = e.eNo")
 	public List<Biz> selectCountTotalFrombiz();
 	
-	@Select("select h.hName as hairStyle, count(b.bhNo) as sumOrder from hairshop.biz b, hairshop.hairInfo h where b.bhNo = h.hNo group by b.bhNo")
+	@Select("select h.hName as hairStyle, count(b.hNo) as sumOrder from hairshop.biz b, hairshop.hairInfo h where b.hNo = h.hNo group by b.hNo")
 	public List<Biz> selectCountStyleForGraph();
 	
 	@Insert("insert into Biz values(#{bDate},#{bTime},#{bcNo},#{bhNo},#{beNo})")
